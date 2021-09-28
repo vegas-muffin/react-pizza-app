43295e7b178cecc5ab127822a9dc4f4a2049e153
Vagiz Musin <vagiz_mus@mail.ru>
2021-09-28 21:10:37 +0600

    feat: add Cart, Home, SortPopup

--- pizza-app/src/components/index.js
+++ pizza-app/src/components/index.js
@@ -1,3 +1,5 @@
 export { default as Header } from './Header';
 export { default as Button } from './Button';
 export { default as Categories } from './Categories';
+export { default as SortPopup } from './SortPopup';
+export { default as Home } from '../pages/Home';
--- pizza-app/src/index.js
+++ pizza-app/src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './scss/app.scss';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Header from './components/Header';
+
 // import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App/>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root'),
 );
