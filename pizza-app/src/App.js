947bcbeca6184121836da0614c9429a71b6ea693
Vagiz Musin <vagiz_mus@mail.ru>
2021-10-03 18:35:39 +0600

    feat: add redux-thunk

--- pizza-app/src/App.js
+++ pizza-app/src/App.js
@@ -1,20 +1,17 @@
 import React from 'react';
-import axios from 'axios';
 
 import { useDispatch } from 'react-redux';
 
 import { Header } from './components/index';
 import { Home, Cart } from './pages/index';
 import { Route } from 'react-router-dom';
-import { setPizzas } from './redux/actions/pizzas';
+import { fetchPizzas } from './redux/actions/pizzas';
 
 function App() {
   const dispatch = useDispatch();
 
   React.useEffect(() => {
-    axios.get('http://localhost:3001/pizzas').then(({ data }) => {
-      dispatch(setPizzas(data));
-    });
+    console.log(dispatch(fetchPizzas()));
   }, []);
 
   return (
