584b739a37590542dcbc0d9b39dedab0f79b218e
Vagiz Musin <vagiz_mus@mail.ru>
2021-10-06 17:14:19 +0600

    feat: add cartitem

--- pizza-app/src/components/index.js
+++ pizza-app/src/components/index.js
@@ -4,5 +4,6 @@ export { default as Categories } from './Categories';
 export { default as SortPopup } from './SortPopup';
 export { default as PizzaBlock } from './PizzaBlock';
 export { default as PizzaLoadingBlock } from './PizzaBlock/LoadingBlock';
+export { default as CartItem } from './CartItem';
 
 export { default as Home } from '../pages/Home';
